ce3c95107ecfe82c4e76dc29f0b7afa7af7210b7
gabi <z5592426@ad.unsw.edu.au>
2026-08-16 11:59:46 +1000

    added /me route to test middleware

---
+++ backend/src/routes/me.routes.ts
@@ -0,0 +1,18 @@
+import { authMiddleware } from '@/middleware/auth.middleware.js';
+import { asyncHandler } from '@/utils/asyncHandler.js';
+import { Router } from 'express';
+
+export const meRouter = Router();
+
+meRouter.get('/me',
+    // passing middleware fn into another fn
+    // pretty much if it returns an err then handles that separately
+    asyncHandler(authMiddleware),
+    (req, res) => {
+
+    // at this pt user alr avaialble
+
+    return res.status(200).json(req.user);
+    
+  }
+);   
