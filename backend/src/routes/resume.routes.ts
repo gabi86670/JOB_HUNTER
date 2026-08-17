65d5a9bf0805449e5b06b1befc92761689587842
gabi <z5592426@ad.unsw.edu.au>
2026-08-17 12:22:12 +1000

    fixed routing for resume upload

--- backend/src/routes/resume.routes.ts
+++ backend/src/routes/resume.routes.ts
@@ -3,7 +3,7 @@ import { uploadResumeService } from "@/services/resumes/resume.service.js";
 import { asyncHandler } from "@/utils/asyncHandler.js";
 import { Router } from "express";
 import { authMiddleware } from '@/middleware/auth.middleware.js';
-
+import { ValidationError } from "@/utils/errors.js";
 export const resumeRouter = Router();
 
 // parses the incoming file and calls uploadResumeService
@@ -11,11 +11,16 @@ export const resumeRouter = Router();
 
 // to init the uploading of resume to the db bucket + local memory
 resumeRouter.post('/upload',
+    // validate user before upload
     asyncHandler(authMiddleware),
-    asyncHandler(uploadResume.single('resume')),
-    (req, file, callback) => {
+    // check file can be uploaded
+    uploadResume.single('resume'),
+    asyncHandler(async (req, res) => {
+        if (!req.file) {
+            throw new ValidationError('Resume file is required');
+        }
         // file successfully checked - ie its right size, saved in local memory etc
-        const res = uploadResumeService(req.user.id, req.file);
-        return res.status(200).json(req.data); 
-    }
+        const resume = await uploadResumeService(req.user.id, req.file);
+        return res.status(201).json(resume); 
+    })
 );
